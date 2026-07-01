64f5fc0423ae5db4244da1f5d018d6a7965498aa
cakethehuman <willsonputra17@gmail.com>
2026-07-01 21:13:44 +0700

    Fixed

--- utils/finance.py
+++ utils/finance.py
@@ -1,11 +1,11 @@
 import logging
 import yfinance as yf
-
+from curl_cffi import requests
 logger = logging.getLogger(__name__)
-
+session = requests.Session(impersonate="chrome")
 def get_ticker_latest_price(name : str) -> float | None:
     try:
-        ticker = yf.Ticker(name)
+        ticker = yf.Ticker(name, session=session)
         price = ticker.info.get('regularMarketPrice')
         return price
     except Exception as e:
